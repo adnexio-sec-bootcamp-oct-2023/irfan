6729352caaa3894cb454544c67887a4ee895a758
funmeen <irfanmuhymeen@gmail.com>
2023-11-10 16:48:40 +0800

    CU-860ry30fn - Day 5 80DC: Introduction to JavaScript: Object Oriented Programming

---
+++ c5/e2/e2.js
@@ -0,0 +1,27 @@
+//class es6
+class Vehicle {
+    constructor(brand, speed = 0) {
+      this.brand = brand;
+      this.speed = speed;
+    }
+  
+    accelerate(acceleration) {
+      this.speed += acceleration;
+      console.log(`${this.brand} is accelerating. Current speed: ${this.speed} km/h`);
+    }
+  
+    brake(deceleration) {
+      if (this.speed - deceleration >= 0) {
+        this.speed -= deceleration;
+      } else {
+        this.speed = 0;
+      }
+      console.log(`${this.brand} is braking. Current speed: ${this.speed} km/h`);
+    }
+  }
+  
+  // Example usage:
+  const myCar = new Vehicle('Toyota', 60);
+  myCar.accelerate(20);  
+  myCar.brake(30);       
+  
